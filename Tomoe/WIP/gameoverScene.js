d6c545e9f7187dc3c558bbdfec5e965f06672852
22ndteam <56058116+22ndteam@users.noreply.github.com>
2021-03-24 18:39:37 +0800

    Add files via upload

---
+++ Tomoe/WIP/gameoverScene.js
@@ -0,0 +1,50 @@
+class gameoverScene extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: 'gameoverScene' });
+    }
+
+    preload() {
+        this.load.image('gameover','assets/Deathscreen.png');
+		this.load.atlas('continue','assets/continue.png','assets/continue.json')
+    }
+
+    create () {
+
+        this.add.image(0, 0, 'gameover').setOrigin(0, 0);
+		
+        this.anims.create({
+            key:'proceedflicker',
+            frames:[
+                {key:'continue',frame:'continue1'},
+                {key:'continue',frame:'continue2'},
+            ],
+            frameRate:1,
+            repeat:-1
+        });
+
+		this.physics.add.sprite(640, 420, 'continue').play('proceedflicker')
+		
+        console.log("This is gameoverScene");
+
+        //this.input.once('pointerdown', function(){
+        var spaceDown = this.input.keyboard.addKey('SPACE');
+        var aDown = this.input.keyboard.addKey('A');
+
+        
+        spaceDown.on('down', function(){
+        console.log("Spacebar pressed, reply game");
+        this.scene.stop("gameoverScene");
+        this.scene.start("level1");
+        }, this );
+
+        aDown.on('down', function(){
+            console.log("A pressed (main menu)");
+            this.scene.stop("gameoverScene");
+            this.scene.start("mainScene");
+            }, this );
+
+    }
+
+}
